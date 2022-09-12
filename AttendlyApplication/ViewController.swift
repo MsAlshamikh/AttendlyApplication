12099aeaaa91e5767314965a56e378642975db54
MsAlshamikh <98359943+MsAlshamikh@users.noreply.github.com>
2022-09-13 00:42:15 +0300

    Initial Commit

---
+++ AttendlyApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AttendlyApplication
+//
+//  Created by SHAMMA  on 17/02/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
