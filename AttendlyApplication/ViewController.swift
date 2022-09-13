216f04a00f7d11f9f5d72b8fc9ee920477a30486
MsAlshamikh <98359943+MsAlshamikh@users.noreply.github.com>
2022-09-13 17:41:57 +0300

    hh

--- AttendlyApplication/ViewController.swift
+++ AttendlyApplication/ViewController.swift
@@ -4,9 +4,9 @@ import UIKit
 class ViewController: UIViewController {
   
   let helper = NFChelper()
-  //var payloadLabel: UILabel!
+  var payloadLabel: UILabel!
 
-    @IBOutlet weak var payloadLabel: UILabel!
+    
 
     override func viewDidLoad() {
     super.viewDidLoad()
@@ -18,15 +18,20 @@ class ViewController: UIViewController {
     button.frame = CGRect(x: 60, y: 200, width: self.view.bounds.width - 120, height: 80)
     self.view.addSubview(button)
     
-    payloadLabel = UILabel(frame: button.frame.offsetBy(dx: 0, dy: 300))
-    payloadLabel.numberOfLines = 10
-    payloadLabel.text = "Scan an NFC Tag"
+   payloadLabel = UILabel(frame: button.frame.offsetBy(dx: 0, dy: 300))
+        payloadLabel.numberOfLines = 10
+        payloadLabel.text = "Scan an NFC Tag" 
     self.view.addSubview(payloadLabel)
   }
   
   func onNFCResult(success: Bool, msg: String) {
     DispatchQueue.main.async {
-      self.payloadLabel.text = "\(self.payloadLabel.text!)\n\(msg)"
+      self.payloadLabel.text = "\(self.payloadLabel.text!)\n\(msg)"//Msg Holder , ==Sections of lecturer
+        let x = self.payloadLabel.text!
+        print("@@@@@@@@@@@@@@@@@")//inner tag
+        print(x)//inner tag
+        
+
     }
   }
   
@@ -34,6 +39,8 @@ class ViewController: UIViewController {
     print("didTapReadNFC")
     self.payloadLabel.text = ""
     helper.onNFCResult = onNFCResult(success:msg:)
+      print("*********ssssss")
+    //  print(helper.onNFCResult as String)
     helper.restartSession()
   }
 
