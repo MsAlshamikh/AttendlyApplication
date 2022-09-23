40792eb23f40ae69b74ea71150a3aeb28d112897
Yumna <67751566+yomna1998@users.noreply.github.com>
2022-09-23 00:28:33 -0700

    yumna edit

--- AttendlyApplication/AdvisorVC.swift
+++ AttendlyApplication/AdvisorVC.swift
@@ -14,63 +14,38 @@ class AdvisorVC: UIViewController {
     var students: [String] = []
     func get() {
             let db = Firestore.firestore()
-            db.collection("classes").whereField("LecturerEmail", isEqualTo: Global.shared.useremailshare ).getDocuments {
+            db.collection("Lectures").whereField("EmailLectures", isEqualTo: Global.shared.useremailshare ).getDocuments {
                 (snapshot, error) in
                 if let error = error {
                     print("FAIL ")
                 }
                 else{
-                    print("SUCCESS")
-                    let actual = snapshot!.documents.first!.get("coursess") as! [String]
-                    //let sects = snapshot!.documents.first!.get("Sectionss") as! [String]
-                    print(actual)
-                    if actual.count == 1 {
-                        let  course = actual.first!
-                        db.collection("studentsByCourse").document(course).collection("students").getDocuments { snapshot, error in
-                            guard let snapshot = snapshot else {
-                                return
-                            }
-                            var students: [String] = []
-                            let docs = snapshot.documents
-                            for i in 0..<docs.count {
-                                let document = docs[i].data()
-                                let name = document["name"] as? String ?? ""
-                                students.append(name)
-                                let label = UIButton(frame: .init(x: self.view.frame.midX-148 , y: 280 + ( Double(i) * 90 ), width: 300, height: 60))
-                                label.setTitle(name, for: .normal)
-                                label.titleLabel?.font = label.titleLabel?.font.withSize(30)
-                                label.setTitleColor(UIColor(red: 20/255, green: 108/255, blue: 120/255, alpha: 2), for: .normal)
-                                label.backgroundColor = UIColor(red: 138/255, green: 176/255, blue: 183/255, alpha: 0.75)
-                                //label.params["course"] = actual[i]
-                                //!!!!!!
-                                //label.tag = Int(sects[i]) ?? 0
-
-                                //label.addTarget(self, action: #selector(self.pressed), for: .touchUpInside)
-                                label.layer.cornerRadius = 0.07 * label.bounds.size.width
-                                self.view.addSubview(label)
-                            }
-                            self.students = students
-                        }
+                    guard let lecturer = snapshot?.documents.first else {
+                        return }
                     
+                    guard let lecturerId = lecturer.get("lecturesID") as? String else { return }
+                    //let sects = snapshot!.documents.first!.get("Sectionss") as! [String]
+                    db.collection("Unistudent").whereField("advisorID", isEqualTo: lecturerId).getDocuments { snapshot, error in
+                        guard let snapshot = snapshot else {
+                            return
+                        }
+                        var students: [String] = []
+                        let docs = snapshot.documents
+                        print("Docs... ", docs)
+                        for i in 0..<docs.count {
+                            let document = docs[i].data()
+                            let name = document["name"] as? String ?? ""
+                            students.append(name)
+                            let label = UIButton(frame: .init(x: self.view.frame.midX-148 , y: 280 + ( Double(i) * 90 ), width: 300, height: 60))
+                            label.setTitle(name, for: .normal)
+                            label.titleLabel?.font = label.titleLabel?.font.withSize(30)
+                            label.setTitleColor(UIColor(red: 20/255, green: 108/255, blue: 120/255, alpha: 2), for: .normal)
+                            label.backgroundColor = UIColor(red: 138/255, green: 176/255, blue: 183/255, alpha: 0.75)
+                            label.layer.cornerRadius = 0.07 * label.bounds.size.width
+                            self.view.addSubview(label)
+                        }
+                        self.students = students
                     }
-//                    for i in 0..<actual.count {
-//
-//                        let label = UIButton(frame: .init(x: self.view.frame.midX-148 , y: 280 + ( Double(i) * 90 ), width: 300, height: 60))
-//                        label.setTitle(actual[i], for: .normal)
-//                        label.titleLabel?.font = label.titleLabel?.font.withSize(30)
-//                        label.setTitleColor(UIColor(red: 20/255, green: 108/255, blue: 120/255, alpha: 2), for: .normal)
-//                        label.backgroundColor = UIColor(red: 138/255, green: 176/255, blue: 183/255, alpha: 0.75)
-//                        //label.params["course"] = actual[i]
-//                        //!!!!!!
-//                        //label.tag = Int(sects[i]) ?? 0
-//
-//                        //label.addTarget(self, action: #selector(self.pressed), for: .touchUpInside)
-//                        label.layer.cornerRadius = 0.07 * label.bounds.size.width
-//                        self.view.addSubview(label)
-//                    }
-                    //Vstack
-                    // coursesT.text = actual
-                    //     print((actual).count)
                 }
             }
 
@@ -79,7 +54,6 @@ class AdvisorVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.navigationController?.isNavigationBarHidden = false
         let resultsController = UITableViewController(style: .grouped)
         let searchController = UISearchController(searchResultsController: resultsController)
         searchController.searchResultsUpdater = self
