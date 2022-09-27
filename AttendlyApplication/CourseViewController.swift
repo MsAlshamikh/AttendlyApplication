91eec6ad560e81310869b3ee9930046ca20210ec
Amani Aldahmash <65466651+amanialdahmash@users.noreply.github.com>
2022-09-27 19:15:41 +0300

    percentage backend done

--- AttendlyApplication/CourseViewController.swift
+++ AttendlyApplication/CourseViewController.swift
@@ -19,8 +19,9 @@ class CourseViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        get()
         percentage()
+      get()
+       
     }
     /*  func loadStats(){
      let db = Firestore.firestore()
@@ -52,7 +53,7 @@ class CourseViewController: UIViewController {
             guard let sectsChk = snapshot.documents.first?.get("Sections") as? [String] else { return }
             var abbsencest = snapshot.documents.first!.get("abbsencest") as! [String: Int]
             print("dict ", abbsencest)
-            
+            var i = 0
             for section in sectsChk {
                 var globalAbbsencen = 0
                 let t_snapshot = try await db.collection("studentsByCourse").whereField("tag", isEqualTo: section).getDocuments()
@@ -118,24 +119,84 @@ class CourseViewController: UIViewController {
                                        totalp[section] = final
                                        
                                        print(final)
+                                       
+                                       
+                                       
+                                       
+                                       
+                                       
+                                       let actual = snapshot!.documents.first!.get("courses") as! [String]
+                                        let sects = snapshot!.documents.first!.get("Sections") as! [String]
+                                   print(actual)
+                                   //for i in 0..<actual.count {
+                                       let label = UIButton(frame: .init(x: self.view.frame.midX-238 , y: 280 + ( Double(i) * 90 ), width: 300, height: 60))
+                                       label.setTitle(actual[i], for: .normal)
+                                       label.titleLabel?.font = label.titleLabel?.font.withSize(30)
+                                       label.setTitleColor(UIColor(red: 20/255, green: 108/255, blue: 120/255, alpha: 2), for: .normal)
+                                      // label.backgroundColor = UIColor(red: 138/255, green: 176/255, blue: 183/255, alpha: 0.75)
+                                       
+                                       //line
+                                       let line =  UIButton(frame: .init(x: Int(self.view.frame.midX)-148 , y: 333 + ( Int((Double(i))) * 90 ), width: 250, height: 26))
+                                
+                                       //
+                                       line.layer.cornerRadius = 0.04 * label.bounds.size.width
+                                       line.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 0.75)
+                                       
+                                       //
+                                       var totalp = snapshot!.documents.first!.get("percentage") as! [String: Double]
+                                       print("this ok up/" , totalp)
+                                       //
+                                       let z = final
+                                       let after = final*10
+                                      //
+                                       let perc = UIButton(frame: .init(x: CGFloat(self.view.frame.midX)-148 , y: Double(333 + ( Int((Double(i))) * 90 )), width: after, height: 26))
+                                       //
+                                       perc.layer.cornerRadius = 0.04 * label.bounds.size.width
+                                       if (z>20)
+                                       {perc.backgroundColor = UIColor(red: 355/255, green: 0/255, blue: 0/255, alpha: 0.75)}
+                                       else if (z>15)
+                                       {  perc.backgroundColor = UIColor(red: 230/255, green: 126/255, blue: 34/255, alpha: 0.75)}
+                                       else if (z>10)
+                                       {   perc.backgroundColor = UIColor(red: 241/255, green: 196/255, blue: 15/255, alpha: 0.75)}
+                                       else
+                                       { perc.backgroundColor = UIColor(red: 0/255, green: 255/255, blue: 0/255, alpha: 0.75)}
+                    //
+                                       let pt = UILabel(frame: .init(x: Int(self.view.frame.midX)+105 , y: 333 + ( Int((Double(i))) * 90 ), width: 70, height: 26))
+                                       pt.textColor = UIColor(red: 20/255, green: 108/255, blue: 120/255, alpha: 2)
+                                       
+                                     
+                                       pt.font = UIFont(name: "systemFont", size: 27.0)
+                                       
+                                       pt.text = " " + String(final) + "%"
+                                       
+                                       
+                                       self.view.addSubview(perc)
+                                       self.view.addSubview(pt)
+
+                                       self.view.addSubview(line)
+                                           //
+                                       label.tag = Int(section) ?? 0
+                                       label.addTarget(self, action: #selector(self.pressed), for: .touchUpInside)
+                                       label.addTarget(self, action: #selector(self.pressed1), for: .touchDown)
+                                       label.addTarget(self, action: #selector(self.pressed2), for: .touchDragExit)
+                                       label.layer.cornerRadius = 0.07 * label.bounds.size.width
+                                       self.view.addSubview(label)
+                                       
+                                       i = i+1
+                                       
+                                   
+     
                                    }
                             
                                }
-
+                    }
                         
                     }
-                
-                
-                //store
-                //  let abbsencest = snapshot.documents.first!.get("abbsencest") as! [Map]
-                //  print("all number abbsecnce/",abbsencest)
-            }
-        }
+      
+            }}}
+    
+    func get() {
         
-        }}
-    
-    
-    func get(){
         let db = Firestore.firestore()
         db.collection("Unistudent").whereField("StudentEmail", isEqualTo: "441201198@student.ksu.edu.sa").getDocuments{
             (snapshot, error) in
@@ -157,6 +218,7 @@ class CourseViewController: UIViewController {
                 
                 //
                 else{
+                    /*
                     let actual = snapshot!.documents.first!.get("courses") as! [String]
                      let sects = snapshot!.documents.first!.get("Sections") as! [String]
                 print(actual)
@@ -174,13 +236,13 @@ class CourseViewController: UIViewController {
                     line.layer.cornerRadius = 0.04 * label.bounds.size.width
                     line.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 0.75)
                     
-                    
                     //
-                    let z = 17
+                    var totalp = snapshot!.documents.first!.get("percentage") as! [String: Double]
+                    print("this ok/" , totalp)
+                    //
+                    let z = Int(totalp[sects[i]]!)
                     let after = z*10
                    //
-                    
-                    
                     let perc = UIButton(frame: .init(x: Int(self.view.frame.midX)-148 , y: 333 + ( Int((Double(i))) * 90 ), width: after, height: 26))
                     //
                     perc.layer.cornerRadius = 0.04 * label.bounds.size.width
@@ -215,16 +277,15 @@ class CourseViewController: UIViewController {
                     self.view.addSubview(label)
                     
                     
-                }}
-                
-               //
+                }}*/
                 //Vstack
                 // coursesT.text = actual
                 //     print((actual).count)
             }
         }
-        
+        }
     }
+                
     @objc func pressed1(sender:UIButton) {
         sender.setTitleColor(UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 2), for: .normal)
        // sender.backgroundColor = UIColor(red: 20/255, green: 108/255, blue: 120/255, alpha: 0.75)
@@ -292,109 +353,3 @@ class CourseViewController: UIViewController {
     
   
 }
-/*
-func checkCoursesExist(email: String, collection: String, field: String) async -> Bool {
-
-    let db = Firestore.firestore()
-
-    do {
-
-        let snapshot = try await db.collection(collection).whereField(field, isEqualTo: email).getDocuments()
-        let scs = snapshot.documents.first!.get("lecturerID") as! String
-        
-        return scs.count != 0
-
-    } catch {
-
-        print(error.localizedDescription)
-
-    }
-
-    return false
-
-}
- }*/
-
-/*
- func get(){
-     let db = Firestore.firestore()
-     Task{
-         do{
-     db.collection("Unistudent").whereField("StudentEmail", isEqualTo: "322@student.ksu.edu.sa").getDocuments{
-         (snapshot, error) in
-         if let error = error {
-             print("FAIL ")
-         }
-         else{
-             print("SUCCESS")
-             if await self.checkCoursesExist(email: "322@student.ksu.edu.sa", collection: "Unistudent", field: "StudentEmail"){
-             let actual = snapshot!.documents.first!.get("courses") as! [String]
-             let sects = snapshot!.documents.first!.get("Sections") as! [String]
-             print(actual)
-             for i in 0..<actual.count {
-                 
-                 let label = UIButton(frame: .init(x: self.view.frame.midX-148 , y: 280 + ( Double(i) * 90 ), width: 300, height: 60))
-                 label.setTitle(actual[i], for: .normal)
-                 label.titleLabel?.font = label.titleLabel?.font.withSize(30)
-                 label.setTitleColor(UIColor(red: 20/255, green: 108/255, blue: 120/255, alpha: 2), for: .normal)
-                 label.backgroundColor = UIColor(red: 138/255, green: 176/255, blue: 183/255, alpha: 0.75)
-                 //label.params["course"] = actual[i]
-                 //!!!!!!
-                 label.tag = Int(sects[i]) ?? 0
-                 label.addTarget(self, action: #selector(self.pressed), for: .touchUpInside)
-                 label.layer.cornerRadius = 0.07 * label.bounds.size.width
-                 self.view.addSubview(label)
-             }
-             //Vstack
-             // coursesT.text = actual
-             //     print((actual).count)
-         }
-             else
-             {let c = UILabel(frame: .init(x: self.view.frame.midX-120 , y: 200 , width: 250, height: 50))
-                 c.title = "No courses"}
-                 //
-             }}}
-         catch {
-         }}}
- */
-
-
-/*
- //PERCENTAGE TRYING
- var temp = 0
-let abs_snapshot =  db.collection("studentsByCourse").whereField("tag", isEqualTo: sects[0])
-     .getDocuments{
-     (snapshot1, error) in
-     if let error = error {
-         print("FAIL ")
-     }
-     else{
-      
-     }
-     }}
- 
-/*   abs_snapshot.data("students").whereField("EmailStudent", isEqualTo: "441201198@student.ksu.edu.sa").getDocuments{
-     (snapshot1, error) in
-     if let error = error {
-         print("FAIL ")
-     }
-     else{
-         
-         let state = snapshot!.documents.first!.get("State") as! String
-         if state == "absent"
-         { temp = temp + 1
-             print(temp)
-         }
-         
-         /*
-          let state = snapshot!.documents.get("State") as! [String]
-          for i in 0..<state.count {
-          if stateA[i] == "absent"
-          { temp = temp + 1
-              print(temp)
-          }
-          */*/
-         
-     }}
- */
-
