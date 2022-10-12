843b293314604e3c134a80c31e4931ca1d240374
Amani Aldahmash <65466651+amanialdahmash@users.noreply.github.com>
2022-10-12 22:04:19 +0300

    edits

---
+++ AttendlyApplication/SPSH.swift
@@ -0,0 +1,32 @@
+//
+//  SPSH.swift
+//  AttendlyApplication
+//
+//  Created by Amani Aldahmash on 12/10/2022.
+//
+
+import UIKit
+
+class SPSH: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        var imageView : UIImageView
+//          imageView  = UIImageView(frame:CGRectMake(10, 50, 100, 300));
+//           imageView.image = UIImage(named:"animated-check.gif")
+//           self.view.addSubview(imageView)
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
