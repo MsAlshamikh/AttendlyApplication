7a9b8c13d25c96adff2a0466a737c12befbca32e
MsAlshamikh <98359943+MsAlshamikh@users.noreply.github.com>
2022-10-26 13:58:55 +0300

    timetable design

--- AttendlyApplication/ViewController2.swift
+++ AttendlyApplication/ViewController2.swift
@@ -108,13 +108,13 @@ print(indexPath)
         cell.backgroundColor = gridLayout.isItemSticky(at: indexPath)  ? .groupTableViewBackground : .white
         if(cell.backgroundColor == .white){
             cell.backgroundColor =  #colorLiteral(red: 0.8768938184, green: 0.8879328966, blue: 0.8877387047, alpha: 1)
-            cell.layer.borderWidth = 0.3
+          //  cell.layer.borderWidth = 0.3
             cell.layer.borderColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
             cell.titleLabel.textColor =  #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         }
         else{
             cell.backgroundColor = #colorLiteral(red: 0.05053991079, green: 0.5733678937, blue: 0.6324701905, alpha: 1)
-            cell.layer.borderWidth = 0.3
+          //  cell.layer.borderWidth = 0.3
             cell.layer.borderColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
             cell.titleLabel.textColor =  #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         }
