499e3ae395dc8ace8961ec9201c6699b05e63a5c
Amani Aldahmash <65466651+amanialdahmash@users.noreply.github.com>
2022-11-01 17:26:46 +0300

    merge1

---
+++ AttendlyApplication/CollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CollectionViewCell.swift
+//  AttendlyApplication
+//
+//  Created by Amani Aldahmash on 01/11/2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    static let reuseID = "CollectionViewCell"
+
+    @IBOutlet weak var titleLabel: UILabel!
+
+}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
