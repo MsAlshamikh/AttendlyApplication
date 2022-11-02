f77252b14afaca826e77e1a33614370370515777
MsAlshamikh <98359943+MsAlshamikh@users.noreply.github.com>
2022-11-02 03:05:26 +0300

    amaaannnnniiii

--- AttendlyApplication/ViewController3.swift
+++ AttendlyApplication/ViewController3.swift
@@ -295,44 +295,130 @@ extension ViewController3: UICollectionViewDelegateFlowLayout , UICollectionView
     }
   
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.reuseID, for: indexPath) as? CollectionViewCell else {
-//           return
-//
-//        }
-      let cell = collectionView.cellForItem(at: indexPath) as! CollectionViewCell
-        Global.shared.WhatPressed = cell.titleLabel2.text!
-        Global.shared.titleB = cell.titleLabel2.text!
-        if(cell.titleLabel2.text! != ""){
+    //        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.reuseID, for: indexPath) as? CollectionViewCell else {
+    //           return
+    //
+    //        }
+          let cell = collectionView.cellForItem(at: indexPath) as! CollectionViewCell
+            Global.shared.WhatPressed = cell.titleLabel2.text!
+                            var studentArry = [String]()
+                            var emailArry = [String]()
+                            var studentID = [String]()
+                            var perecmtageArrya = [String]()
+            var  v =  ""
+            var  i  =  0
+         if(cell.titleLabel2.text! != ""){
+             v = cell.titleLabel2.text!
+              let db = Firestore.firestore()
+             
+     db.collection("Unistudent").whereField("courses", arrayContains: v).getDocuments{
+         (snapshot, error) in
+         if let error = error {
+         }
+         else{
+             print("AMANI  BEFORE THE   FOR  LOOOOOP ")
+             for documents in snapshot!.documents
+             {
+                 print("AMANI  IS INSIDE ")
+                 let name = documents.get("name") as! String
+                 print("AMANI  GOT NAME", name)
+                 studentArry.append(name)
+                 print("AMANI  APPEND NAME")
+                 let ID = documents.get("studentID") as! String
+                 print("AMANI  GOT ID", ID)
+                 studentID.append(ID)
+                 let EMAIL = documents.get("StudentEmail") as! String
+                 print("AMANI  GOT EMAIL", EMAIL)
+                 emailArry.append(EMAIL)
+                 
+                 var SA = documents.get("co") as! [String]
+                 var numsec = SA[i+1].split(separator: "-")[1]
+                 
+                 var abbsencest = documents.get("abbsencest") as! [String: Int]
+                 var sectionH = documents.get("sectionH") as! [String: Int]
+                 var percentage = documents.get("percentage") as! [String: Int]
+
+                 print("abbsencest",abbsencest)
+                 var sherdabbsencest = 0
+                 var sheredsectionH = 0
+                 var sheredpercentage = 0
+               //  for valueAbb in abbsencest {
+                 for (key,value) in abbsencest {
+                     print("\(key): \(value)")
+                     var sectionNumber = key
+                     var abbsentNumber = value
+                     print("sectionNumber",sectionNumber)
+                     print("abbsentNumber",abbsentNumber)
+
+                     if( sectionNumber == numsec ){
+                        print(" just for section that was perssed abbsence", abbsentNumber)
+                         sherdabbsencest = abbsentNumber
+                     }
+
+                 }
+                 for (key,value) in sectionH {
+                     print("\(key): \(value)")
+                     var sectionNumber2 = key
+                     var abbsentNumber2 = value
+                     print("sectionNumber2",sectionNumber2)
+                     print("abbsentNumber2",abbsentNumber2)
+
+                     if( sectionNumber2 == numsec ){
+                        print(" just for section that was perssed abbsence", abbsentNumber2)
+                         sheredsectionH = abbsentNumber2
+                      // perecmtageArrya.append( abbsentNumber
+                     }
+
+                 }
+                 for (key,value) in percentage {
+                     print("\(key): \(value)")
+                     var sectionNumber3 = key
+                     var abbsentNumber3 = value
+                     print("sectionNumber2",sectionNumber3)
+                     print("abbsentNumber2",abbsentNumber3)
+
+                   if( sectionNumber3 == numsec ){
+                        print(" just for section that was perssed abbsence", abbsentNumber3)
+                      // perecmtageArrya.append( abbsentNumber )
+                       sheredpercentage = abbsentNumber3
+
+                     }
+                 }
+                 var step1 = Double(sheredsectionH ) * 0.25
+                    var step2 = ( Double(sherdabbsencest) /  step1 ) * 100
+                   var final = step2 * 0.25
+                    final = Double(round(10 * final) / 10 )
+                 let st = String(final)
+               perecmtageArrya.append(st)
+             }
             
-            let db = Firestore.firestore()
-            db.collection("Sections").whereField("courseName", isEqualTo: cell.titleLabel2.text!).getDocuments{
-                (snapshot, error) in
-                if let error = error {
-                }
-                else{
-                    let id = snapshot!.documents.first!.get("lecturerID") as! String
-                    Global.shared.section = snapshot!.documents.first!.get("section") as! String
-                    db.collection("Lecturer").whereField("id", isEqualTo: id).getDocuments{
-                        (snapshot, error) in
-                        if let error = error {
-                        }
-                        else{
-                            Global.shared.name = snapshot!.documents.first!.get("name") as! String
-                            if true
-                            { let vc = self.storyboard?.instantiateViewController(withIdentifier: "listAll") as! listAll
-                                //vc.nameStudent = studentArry
-                              //  vc.idStudent = studentID
-                              //  vc.v = v
-                             //   vc.emailStudent = emailArry
-                             //   vc.percentagestu = perecmtageArrya
-                                self.navigationController?.pushViewController( vc, animated: true)}
-                        }
-                    }
-                    }
-                }
-            
-            }}
-}
+         
+
+    //                  for document in t_snapshot.documents {
+    //                   // print(course)
+    //                    print("here")
+    //                      let name = document.get("name") as! String
+    //                      let ID = document.get("studentID") as! String
+    //                      let EMAIL = document.get("StudentEmail") as! String
+    //                      studentArry.append(name)
+    //                      studentID.append(ID)
+    //                      emailArry.append(EMAIL)
+                          
+                               let vc1 = self.storyboard?.instantiateViewController(withIdentifier: "listAll") as! listAll
+             
+            print("AMANI  LETS  SEE", studentArry)
+             print("AMANI  LETS  SEE ID", studentID)
+             print("AMANI  LETS  SEE v", v)
+             
+                                vc1.nameStudent = studentArry
+                                 vc1.idStudent = studentID
+                                   vc1.v = v
+                                   vc1.emailStudent = emailArry
+                                   vc1.percentagestu = perecmtageArrya
+                                    self.navigationController?.pushViewController( vc1, animated: true)}
+     }}}}
+
+
 
 
 
