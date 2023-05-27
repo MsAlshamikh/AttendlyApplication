7d4a13e0fe9be2e83ee1ba87528813fdcf38e44c
Amani Aldahmash <65466651+amanialdahmash@users.noreply.github.com>
2023-05-27 17:16:12 +0300

    amani455

--- AttendlyApplication/AdvisorVC.swift
+++ AttendlyApplication/AdvisorVC.swift
@@ -96,25 +96,26 @@ class AdvisorVC: UIViewController {
                 self.view.addSubview(label)
             }
         } else {
-            showNoStudentsMessage()
+            let label = UIButton(frame: .init(x: self.view.frame.midX-175 , y: 340 + ( Double(0) * 90 ), width: 350, height: 60))
+            label.setTitle(" No Student Found", for: .normal)
+            label.titleLabel?.font = label.titleLabel?.font.withSize(30)
+            label.setTitleColor(UIColor(red: 20/255, green: 108/255, blue: 120/255, alpha: 2), for: .normal)
+            label.tag = 999
+            self.view.addSubview(label)
         }
     }
     
     func toggleNoStudentsFound(show: Bool) {
         if(show) {
-            showNoStudentsMessage()
+            let label = UIButton(frame: .init(x: self.view.frame.midX-175 , y: 500 + ( Double(0) * 90 ), width: 350, height: 60))
+            label.setTitle(" No Student Found", for: .normal)
+            label.titleLabel?.font = label.titleLabel?.font.withSize(30)
+            label.setTitleColor(UIColor(red: 20/255, green: 108/255, blue: 120/255, alpha: 2), for: .normal)
+            label.tag = 999
+            self.view.addSubview(label)
         } else {
             removeStudents()
         }
-    }
-    
-    func showNoStudentsMessage() {
-        let label = UIButton(frame: .init(x: self.view.frame.midX-175 , y: 500 + ( Double(0) * 90 ), width: 350, height: 60))
-        label.setTitle(" No Student Found", for: .normal)
-        label.titleLabel?.font = label.titleLabel?.font.withSize(30)
-        label.setTitleColor(UIColor(red: 20/255, green: 108/255, blue: 120/255, alpha: 2), for: .normal)
-        label.tag = 999
-        self.view.addSubview(label)
     }
     
     func removeStudents() {
